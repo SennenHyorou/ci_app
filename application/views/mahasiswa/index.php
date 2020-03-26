09250f12198ae8238675dc6d9c72714224ae2969
Chen Indra <chenindraxr2@gmail.com>
2020-03-26 21:18:01 +0700

    Menganti popup biasa menjadi memakai fitur sweetalert 2

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -1,6 +1,7 @@
 <div class="container">
+	<div class="flash-data" data-flashdata="<?= $this->session->flashdata('flash'); ?>"></div>
 	<?php if($this->session->flashdata('flash') ) :?>
-	<div class="row mt-3">
+	<!-- <div class="row mt-3">
 		<div class="col-md-6">
 			<div class="alert alert-success alert-dismissible fade show" role="alert">
  			 Data mahasiswa <strong>Berhasil</strong> <?= $this->session->flashdata('flash'); ?>
@@ -9,7 +10,7 @@
   			 </button>
 			</div>
 		</div>
-	</div>
+	</div> Bisa pake command (CTRL + / )-->
 	<?php endif; ?>
 
 	<div class="row mt-3">
@@ -43,7 +44,7 @@
 				<?php foreach ($mahasiswa as $mhs) : ?>
 					  <li class="list-group-item">
 					  	<?= $mhs['nama']; ?>
-					  		<a href="<?= base_url(); ?>mahasiswa/hapus/<?= $mhs['id'];?>" class="badge badge-danger float-right" onclick="return confirm('Yakin?');">Hapus</a>
+					  		<a href="<?= base_url(); ?>mahasiswa/hapus/<?= $mhs['id'];?>" class="badge badge-danger float-right tombol-hapus">Hapus</a>
 					  		<a href="<?= base_url(); ?>mahasiswa/ubah/<?= $mhs['id'];?>" class="badge badge-success float-right">Ubah</a>
 					  		<a href="<?= base_url(); ?>mahasiswa/detail/<?= $mhs['id'];?>" class="badge badge-primary float-right">Detail</a>
 					  	</li>
